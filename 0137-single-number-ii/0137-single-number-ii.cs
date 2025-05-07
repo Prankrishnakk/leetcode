db9519dba859dbdfa6be76a0e0fe5d10109f3d85
Pran Krishna kk <prankrishnakk340@gmail.com>
2025-05-07 17:47:29 +0530

    Time: 10 ms (8.5%), Space: 45.7 MB (6.07%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        var result = nums
+             .GroupBy(x => x)
+             .Where(g => g.Count() == 1)
+            .Select(g => g.Key)
+            .First();
+
+        return result;
+    }
+}
